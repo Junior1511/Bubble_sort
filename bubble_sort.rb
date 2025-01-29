60ae8b9ddbf4b4bfbc98b2b77fb2bd1e978b04e2
Junior1511 <yunior122000@gmail.com>
2025-01-28 22:02:15 -0600

    holy this took me a long time to do, because I miss understood something really bad, anyway, I finally get to move on! Kind of crazy

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,5 +1,18 @@
-def bubble_sort (array)
-  array.sort {|a, b| a <=> b }
-end
+# def bubble_sort (array)
+#   array.sort {|a, b| a <=> b }
+# end
+#  They said not to do this, so i guess
+#   I am making a different one
 
-p bubble_sort([4,3,78,2,0,2])
+def bubble_sort (array)
+    length = array.length
+    length.times do
+        for i in 0..(length - 2)
+          if array[i] > array[i + 1]
+            array[i], array[i + 1] = array[i + 1],array[i]
+          end
+        end
+    end
+    array
+end
+p bubble_sort([7,8,9,4,3,78,2,0,2])
