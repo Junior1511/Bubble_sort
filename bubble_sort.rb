e19c69ed6868d9b594a154c486428cf7547bbee4
Junior1511 <yunior122000@gmail.com>
2025-12-14 20:36:41 -0600

    its kind of crazy, sometimes I feel like I am not good at this, I am fixing all of the problems but dude, idk it just seems like im strugling but maybe that is part of the journey, I wont quit. ***** YOU GOT THIS MAN DONT QUIT *****

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,15 +1,21 @@
 def bubble_sort (array)
-  
+        i = 0
+        array.length.times do
+            loop do
+                if i == (array.length - 1)
+                    i = 0
+                elsif array[i] > array[i + 1]
+                  alternator = array[i]
+                  array[i] = array[i + 1]
+                  array[i + 1] = alternator
+                else 
+                    i += 1
+                end
+                break if i == (array.length - 1)
+            end
+        end
+    p array
 end
 
 
 bubble_sort([4,3,78,2,0,2])
-
-# so, I think for loop is the best way to do this
-# make a loop that compares array[i] aka one item to array[i+1] 
-# if left is bigger than right swap them
-# for that ill have to introduce a 3 variables 
-# so I don't end up losing the value of one of the array items
-# it should technically iterate array.length-1 
-# but ill also add a thing that checks if the array 
-# changed in comparison to the previous iteration
